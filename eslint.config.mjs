9d694c00cf4ff4bb9ef75f59d7fd3076ed1e6c6c
Vichea69 <smart.vichea@gmail.com>
2025-02-21 18:18:59 +0700

    Set up basic Express server with configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    env: {
+      node: true,
+      es2021: true,
+    },
+  },
   pluginJs.configs.recommended,
 ];
