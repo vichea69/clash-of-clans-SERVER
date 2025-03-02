7afa45370abca821d25d8043a7a8122660dbb9b6
Vichea69 <smart.vichea@gmail.com>
2025-03-03 00:22:22 +0700

    Add image compression middleware to base and public base routes
    - Integrate compressImage middleware in base and public base routes
    - Apply image compression for both create and update operations
    - Import compressImage from upload middleware

--- routes/base.route.js
+++ routes/base.route.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { createBase, getBases, getBaseById, updateBase, deleteBase } from '../controllers/base.controller.js';
 import authorize from '../middleware/auth.middleware.js';
-import upload from '../middleware/upload.middleware.js';
+import { upload, compressImage } from '../middleware/upload.middleware.js';
 
 const baseRouter = Router();
 
-baseRouter.post('/', authorize, upload.single('image'), createBase);
+baseRouter.post('/', authorize, upload.single('image'), compressImage, createBase);
 baseRouter.get('/', getBases);
 baseRouter.get('/:id', getBaseById);
-baseRouter.put('/:id', authorize, upload.single('image'), updateBase);
+baseRouter.put('/:id', authorize, upload.single('image'), compressImage, updateBase);
 baseRouter.delete('/:id', authorize, deleteBase);
 
 export default baseRouter; 
--- routes/publicBase.route.js
+++ routes/publicBase.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createPublicBase, getPublicBases, getPublicBaseById, updatePublicBase, deletePublicBase } from '../controllers/publicBase.controller.js';
-import upload from '../middleware/upload.middleware.js';
+import { upload, compressImage } from '../middleware/upload.middleware.js';
 import { clerkMiddleware } from '@clerk/express';
 
 const publicBaseRouter = Router();
@@ -10,8 +10,8 @@ publicBaseRouter.get('/', getPublicBases);
 publicBaseRouter.get('/:id', getPublicBaseById);
 
 // Protected routes - require authentication
-publicBaseRouter.post('/', clerkMiddleware(), upload.single('image'), createPublicBase);
-publicBaseRouter.put('/:id', clerkMiddleware(), upload.single('image'), updatePublicBase);
+publicBaseRouter.post('/', clerkMiddleware(), upload.single('image'), compressImage, createPublicBase);
+publicBaseRouter.put('/:id', clerkMiddleware(), upload.single('image'), compressImage, updatePublicBase);
 publicBaseRouter.delete('/:id', clerkMiddleware(), deletePublicBase);
 
 export default publicBaseRouter; 
